e0c86d252c077637fddcd7af1bd07aaf04db95d2
jokerpumpkin <2939223980@qq.com>
2019-06-28 21:36:50 +0800

    将日志由log4j改为log4j2

--- src/main/java/top/nanli/template/TemplateApplication.java
+++ src/main/java/top/nanli/template/TemplateApplication.java
@@ -1,5 +1,9 @@
 package top.nanli.template;
 
+
+
+
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -8,13 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TemplateApplication {
 
-    private static Logger logger = LoggerFactory.getLogger(TemplateApplication.class);
-
     public static void main(String[] args) {
-        logger.info("SpringBoot启动中...");
-        for (int i = 0; i < 10; i++) {
-            System.out.println(" "+i);
-        }
         SpringApplication.run(TemplateApplication.class, args);
     }
 
